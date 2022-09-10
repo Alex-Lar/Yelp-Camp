b81a2b403106dc1a854f71c73034381f395158c4
Alexander Larin <doremilarin@gmail.com>
2022-09-10 09:04:44 +0300

    added mapbox maps into show and index routes

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,13 @@
 const Campground = require('../models/campground');
 
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding')
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({accessToken: mapBoxToken});
+
+const { storage, cloudinary } = require('../cloudinary');
+
+
+
 module.exports.index = async(req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
@@ -10,10 +18,17 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async(req, res, next) => {
-    let campground = new Campground(req.body.campground);
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send();
+
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+    // campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
     campground.author = req.user._id;
     await campground.save();
-
+    console.log(campground);
     req.flash('success', 'Successfully made a new campground!');
     
     res.redirect(`/campgrounds/${campground._id}`);   
@@ -51,7 +66,32 @@ module.exports.renderEditForm = async(req, res) => {
 
 module.exports.updateCampground = async(req, res) => {
     const {id} = req.params;
+    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+
+    const images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.images.push(...images);
+    await campground.save();
+
+    if (req.body.deleteImages) {
+        // Удаляем с cloudinary
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+
+        // Удаляем из базы данных
+        await campground.updateOne({
+            $pull: {
+                images: {
+                    filename: {
+                        $in: req.body.deleteImages
+                    }
+                }
+            }
+        });
+        console.log(campground);
+    }
+    
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 };
